c387f91263540e8a5abfdaa274206d51adccca95
SynMobUsr <arsv@inbox.lv>
2017-08-21 02:34:51 +0300

    new commit_21082017_1

--- src/main/java/August/javaProject/LibraryApplication.java
+++ src/main/java/August/javaProject/LibraryApplication.java
@@ -2,38 +2,30 @@ package August.javaProject;
 
 import August.javaProject.Views.DonateBookView;
 import August.javaProject.Views.Views;
-import August.javaProject.businessLogic.BookEntryValidator;
-import August.javaProject.businessLogic.BusinessLogic;
-import August.javaProject.businessLogic.BusinessLogicImpl;
-import August.javaProject.dataBase.DataBase;
-import August.javaProject.dataBase.DataBaseImpl;
-import August.javaProject.domain.Book;
+import August.javaProject.businessLogic.config.SpringAppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/**
- * Created by SynMobUsr on 8/2/2017.
- */
+
 public class LibraryApplication {
 
 
-    public static void main(String argss[]) {
+    public static void main(String args[]) {
 
-        Book newBook = new Book("Master i Margarita", "Bulgakov", 1934);
-        System.out.println("HashCode of this book is "+newBook.hashCode());
-        DataBaseImpl dataBase = new DataBaseImpl();
-        BookEntryValidator validator = new BookEntryValidator(dataBase);
-        BusinessLogic businessLogic=new BusinessLogicImpl(dataBase, validator);
 
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringAppConfig.class);
         Map<Integer, Views> comands = new HashMap<Integer, Views>();
-        comands.put(1, new DonateBookView(businessLogic));
+        comands.put(1, applicationContext.getBean(DonateBookView.class));
 
 
         while (true) {
             printLibraryMenu();
-            int userSelection = getUsersSelection();
+            int userSelection = getUserSelection();
             if (userSelection == 5) {
                 break;
             } else {
@@ -54,7 +46,7 @@ public class LibraryApplication {
 
     }
 
-    static Integer getUsersSelection() {
+    static Integer getUserSelection() {
         System.out.println("Select from 1 to 5 and press Enter");
         Scanner sc = new Scanner(System.in);
         return Integer.parseInt(sc.nextLine());
